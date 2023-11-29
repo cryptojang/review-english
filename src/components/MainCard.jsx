a46e0bb2bcac29ee909879ea6ef8c77ae2cf9639
cryptojang <robert.crypto.jang@gmail.com>
2023-11-29 17:26:14 +0900

    add tts

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const MainCard = ({ title, day }) => {
   return (
     <Link to={`/${day}`}>
-      <li className="bg-white w-full py-4 rounded-xl pl-4 shadow-xl  mb-4 text-xl hover:text-white hover:bg-green-700">
-        <span className="bg-red-100 py-2 px-2 rounded-lg text-green-800 font-semibold mr-2  ">
+      <li className="bg-white w-full py-4 rounded-xl pl-4 shadow-xl  mb-4 text-xl hover:text-white hover:bg-green-400">
+        <span className="py-2 px-2 rounded-lg text-green-800 font-semibold mr-2  ">
           Day {day}
         </span>
         <span>{title}</span>
--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -1,12 +1,28 @@
 import { Link, useParams } from "react-router-dom";
 import englishData from "../englishData.json";
 import { useEffect, useState } from "react";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaStar } from "react-icons/fa";
+import axios from "axios";
+
+console.log(process.env.REACT_APP_API_KEY);
 
 const Day = () => {
   const [dailyData, setDailyData] = useState();
   const [isVisibile, setIsVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const isImportant = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
 
   const { day } = useParams();
 
@@ -23,6 +39,62 @@ const Day = () => {
     } else {
       setCurrentPage(currentPage + 1);
       // console.log(currentPage);
+    }
+  };
+
+  const onClickIsImportant = () => {
+    isImportant[currentPage] = true;
+
+    console.log(isImportant[currentPage]);
+    console.log(currentPage);
+    console.log(isImportant);
+  };
+
+  const onClickSound = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isLoading) return;
+
+      const response = await axios.post(
+        `https://texttospeech.googleapis.com/v1/text:synthesize?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          input: {
+            text: dailyData.questions[currentPage].Answer,
+          },
+          voice: {
+            languageCode: "en-us",
+            name: "en-us-Standard-A",
+            ssmlGender: "FEMALE",
+          },
+          audioConfig: {
+            audioEncoding: "MP3",
+            speakingRate: 1,
+            pitch: 1,
+          },
+        }
+      );
+
+      const binaryData = atob(response.data.audioContent); //base64를 atob로 binary 형태로 바꿈
+
+      const byteArray = new Uint8Array(binaryData.length); //binary  파일을 바이트 단위로 쪼개 블롭 하기 쉽게 형태 변환
+
+      for (let i = 0; i < binaryData.length; i++) {
+        byteArray[i] = binaryData.charCodeAt(i);
+      }
+
+      const blob = new Blob([byteArray.buffer], { type: "audio/mp3" });
+
+      const newAudio = new Audio(URL.createObjectURL(blob));
+
+      document.body.appendChild(newAudio);
+      newAudio.play();
+
+      setTimeout(() => setIsLoading(false), 5000);
+    } catch (error) {
+      console.error(error);
+
+      setIsLoading(false);
     }
   };
 
@@ -52,45 +124,62 @@ const Day = () => {
   return (
     <div className="bg-green-100 container relative">
       <div className="absolute top-0 left-0 p-8">
-        <Link to={"/"} className=" hover:text-pink-600 ">
+        <Link to={"/"} className=" hover:text-green-400 ">
           <FaHome size={36} />
         </Link>
       </div>
+
       <h1 className="text-center text-2xl font-semibold text-green-800 mb-12">
         Day {dailyData.day} - {dailyData.title}
       </h1>
-      <div className="mb-4">
+      <div className="mb-4 text-xl  font-semibold relative">
+        <button
+          onClick={onClickIsImportant}
+          className={` absolute -top-8 left-0 ${
+            isImportant[currentPage] && "bg-pink-500"
+          }`}
+        >
+          <FaStar />
+        </button>
         <span className="mr-1">Q</span>
         <span className="mr-2">{currentPage + 1}</span>
 
         {dailyData.questions[currentPage].Question}
       </div>
       <div
-        className={`${!isVisibile && "bg-black"} bg-white rounded-xl h-24 p-4`}
+        className={`${!isVisibile && "bg-black"} ${
+          isVisibile && "bg-white text-green-800 font-semibold"
+        }  rounded-xl h-24 p-4 text-lg`}
       >
         {dailyData.questions[currentPage].Answer}
       </div>
-      <div className="mt-4">
+      <div className="mt-4 flex justify-between">
         <button
-          className="btn-style hover:bg-black hover:text-white"
+          className="btn-style hover:bg-green-400 hover:border-green-400 hover:text-white"
           onClick={onClickPrev}
         >
           Prev
         </button>
+        <div>
+          <button
+            className="btn-style ml-2 hover:bg-green-400 hover:border-green-400 hover:text-white"
+            onClick={() => setIsVisible(!isVisibile)}
+          >
+            {isVisibile ? "가리기" : "답변 보기"}
+          </button>
+          <button
+            className="btn-style ml-2 hover:bg-green-400 hover:border-green-400 hover:text-white"
+            onClick={onClickSound}
+          >
+            외우기
+          </button>
+        </div>
+
         <button
-          className="btn-style ml-2 hover:bg-black hover:text-white"
+          className="btn-style ml-2 hover:bg-green-400 hover:border-green-400 hover:text-white"
           onClick={onClickNext}
         >
           Next
-        </button>
-        <button
-          className="btn-style ml-2 hover:bg-black hover:text-white"
-          onClick={() => setIsVisible(!isVisibile)}
-        >
-          {isVisibile ? "가리기" : "답변 보기"}
-        </button>
-        <button className="btn-style ml-2 hover:bg-black hover:text-white">
-          Sound
         </button>
       </div>
     </div>
